4521cd979b10cf854f036e411d609dc024f8005e
infernalxdrm <privet.al84@gmail.com>
2022-05-13 15:01:21 +0700

    Пятая работа

--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
+    [Header("Прозрачный объект")]
+    [SerializeField]
+    [Min(0)]
     private float changeSpeed;
-
+    
+    [SerializeField]
+    [Min(0)]
     private float defaultAlpha;
+    [SerializeField]
     private Material mat;
+    [SerializeField]
     private bool toDefault;
 
     private void Start()
@@ -19,6 +26,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Сделать прозрачным")]
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
